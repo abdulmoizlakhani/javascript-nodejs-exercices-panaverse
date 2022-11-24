ebbcbf8cf5b574456dd501758258faf3b134f95d
Abdul Moiz Lakhani <alakhani@nisum.com>
2022-11-24 14:51:46 +0500

    - number eight

---
+++ 07-number-eight.mjs
@@ -0,0 +1,20 @@
+const add = (num) => {
+  return 4 + num;
+};
+
+const subtract = (num) => {
+  return 12 - num;
+};
+
+const multiply = (num) => {
+  return 2 * num;
+};
+
+const divide = (num) => {
+  return 32 / num;
+};
+
+console.log("Addition: ", add(4));
+console.log("Subtraction: ", subtract(4));
+console.log("Multiplication: ", multiply(4));
+console.log("Division: ", divide(4));
