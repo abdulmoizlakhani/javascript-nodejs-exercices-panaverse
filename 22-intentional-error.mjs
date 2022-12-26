11d40519c24e197f7c7fd03b3c9fcf40ce094181
Abdul Moiz Lakhani <alakhani@nisum.com>
2022-12-26 19:48:33 +0500

    - add comments

--- 22-intentional-error.mjs
+++ 22-intentional-error.mjs
@@ -1,3 +1,4 @@
 const cars = ["Mercedes", "BMW"];
 
-console.log(cars[2].toLowerCase())
+console.log(cars[2].toLowerCase()) // Error Reason: Index 2 doesn't exist.
+//               ^ It should be 0 or 1 as we have only 2 items in cars array.
