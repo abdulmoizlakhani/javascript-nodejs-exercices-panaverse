8afc53974bb78f66666241e36a04dd8afd181d1f
Abdul Moiz Lakhani <alakhani@nisum.com>
2022-12-27 21:29:07 +0500

    - Alien Colors #1

---
+++ 25-alien-colors-#1.mjs
@@ -0,0 +1,16 @@
+let alien_color = "green";
+
+// Write an if statement to test whether the alien’s color is green.
+// If it is, print a message that the player just earned 5 points.
+if (alien_color === "green") {
+  console.log("Hurray! You earned 5 Point!");
+}
+
+// Write one version of this program that passes the if test and another that fails.
+// (The version that fails will have no output.)
+alien_color = "yellow";
+
+if (alien_color === "yellow") {
+  console.log("Be Careful!");
+} else if (alien_color === "red") {
+}
