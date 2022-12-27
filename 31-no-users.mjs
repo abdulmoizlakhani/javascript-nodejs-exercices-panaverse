94f7add694f404d6f59ff68c6e78b9f76520ffc4
Abdul Moiz Lakhani <alakhani@nisum.com>
2022-12-27 23:07:16 +0500

    - no users

---
+++ 31-no-users.mjs
@@ -0,0 +1,8 @@
+const usernames = [];
+
+// If the list is empty, print the message We need to find some users!
+// Remove all of the usernames from your array, and make sure the correct message is printed.
+
+if (!usernames.length) {
+  console.log("We need to find some users!");
+}
