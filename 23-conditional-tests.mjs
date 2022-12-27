d27debeaaa6b40225640afa3dbaa0784cd5d1f76
Abdul Moiz Lakhani <alakhani@nisum.com>
2022-12-27 20:36:05 +0500

    - conditional tests

---
+++ 23-conditional-tests.mjs
@@ -0,0 +1,40 @@
+const fruit = "pineapple";
+const vehicle = "airplane";
+const laptop = "macbook";
+const profession = "freelance developer";
+const job = "owner";
+const business = "software house";
+const business_region = "worldwide";
+const hobby = "reading technology books";
+const passion = "learn, earn and train others";
+const fashion = "simple";
+
+console.log("Is fruit === 'apple'? I predict false.");
+console.log(fruit === "apple");
+
+console.log("\nIs vehicle === 'airplane'? I predict true.");
+console.log(vehicle === "airplane");
+
+console.log("\nIs laptop === 'hp'? I predict false.");
+console.log(laptop === "hp");
+
+console.log("\nIs profession === 'freelance developer'? I predict true.");
+console.log(profession === "freelance developer");
+
+console.log("\nIs job === 'software engineer'? I predict false.");
+console.log(job === "software engineer");
+
+console.log("\nIs business === 'software house'? I predict true.");
+console.log(business === "software house");
+
+console.log("\nIs business_region === 'Pakistan'? I predict false.");
+console.log(business_region === "Pakistan");
+
+console.log("\nIs hobby === 'reading technology books'? I predict true.");
+console.log(hobby === "reading technology books");
+
+console.log("\nIs passion === 'procrastination'? I predict false.");
+console.log(passion === "procrastination");
+
+console.log("\nIs fashion === 'simple'? I predict true.");
+console.log(fashion === "simple");
