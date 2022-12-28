d80de86e7084ff65632931c70bf3527fef62063b
Abdul Moiz Lakhani <alakhani@nisum.com>
2022-12-28 16:21:59 +0500

    - ordinal numbers

---
+++ 33-ordinal-numbers.mjs
@@ -0,0 +1,17 @@
+// Store the numbers 1 through 9 in a array.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Loop through the array.
+// Use an if-else chain inside the loop to print the proper ordinal ending for each number.
+// Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line
+numbers.forEach((number) => {
+  if (number === 1) {
+    console.log(`${number}st\n`);
+  } else if (number === 2) {
+    console.log(`${number}nd\n`);
+  } else if (number === 3) {
+    console.log(`${number}rd\n`);
+  } else {
+    console.log(`${number}th\n`);
+  }
+});
